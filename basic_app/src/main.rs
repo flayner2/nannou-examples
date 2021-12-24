8719dc644f78b44a2df62c4a04f55036606c42c8
flayner2 <flayner5@gmail.com>
2021-12-23 21:20:23 -0300

    Added gitignore and example of a nannou app skeleton

---
+++ basic_app/src/main.rs
@@ -0,0 +1,21 @@
+use nannou::prelude::*;
+
+struct Model {} // State of the application
+
+fn main() {
+    nannou::app(model) // Start building the app and specify model
+        .event(event) // Specify that we want to handle app events with `event()`
+        .simple_window(view) // Request a simple window to which we'll draw with `view()`
+        .run(); // Run the app
+}
+
+// Runs once at the beginning of the program (e.g. `setup()`)
+// Creates a new instance of the model
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+// Runs every time an app event happens (e.g. key and mouse presses)
+fn event(_app: &App, _model: &mut Model, _event: Event) {}
+
+fn view(_app: &App, _model: &Model, _frame: Frame) {}
